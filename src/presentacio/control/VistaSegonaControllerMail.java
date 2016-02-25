c3e831fb62af7da312725bce45f2e75b2fb1b590
Jahel Carmona Vila <jaheleta@gmail.com>
2016-02-25 16:17:05 +0100

    Treball sobre el controlador del Mail

--- src/presentacio/control/VistaSegonaControllerMail.java
+++ src/presentacio/control/VistaSegonaControllerMail.java
@@ -6,10 +6,28 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class VistaSegonaControllerMail implements Initializable {
+	private static String missatge;
+	@FXML private TextArea quadreMissatge;
+    @FXML private RadioButton radioAlumnestutors;
+    @FXML private RadioButton radioProfessors;
 	
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {}
+	public void initialize(URL location, ResourceBundle resources) {
+		//String aux = quadreMissatge.getText();
+		ToggleGroup gp = new ToggleGroup();
+		radioAlumnestutors.setToggleGroup(gp);
+		radioProfessors.setToggleGroup(gp);
+		gp.selectedToggleProperty().addListener((obser, vell, nou) -> {
+			
+		});
+	}
 	@FXML void enrere(ActionEvent event) {VistaNavigator.loadVista(VistaNavigator.VISTAINI);}
+	
+	
 }
