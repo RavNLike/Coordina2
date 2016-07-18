9f4977035458f15954e6173d72c8a6a9ab6a3d03
RavN <RavN@Mahalanobi>
2016-07-18 16:18:43 +0200

    Commit extra

--- src/bll/ComptadorActes.java
+++ src/bll/ComptadorActes.java
@@ -21,31 +21,42 @@ public class ComptadorActes {
 	}
 	
 	/*
-	 * NECESSITEM DE LA LLIBRERIA JXL
-	 */
+	Nom i valor dels registres pertinents
+	col_prof : 9
+	col_tutor1 : 7
+	col_tutor2 : 8
+	col_tutelats_ini : 11
+	col_tutelats_fi : 26*/
+
+	
 	public void comptarActesMes(String path) throws BiffException, IOException{
+		//obtinc el valor de les columnes
+		LectorRegistres instancia = LectorRegistres.getInstancia();
+		int col_tutor1 = Integer.valueOf(instancia.getValorRegistre("col_tutor1"));
+		int col_tutor2 = Integer.valueOf(instancia.getValorRegistre("col_tutor2"));
+		int col_prof = Integer.valueOf(instancia.getValorRegistre("col_prof"));
+		int col_tutelats_ini = Integer.valueOf(instancia.getValorRegistre("col_tutelats_ini"));
+		int col_tutelats_fi = Integer.valueOf(instancia.getValorRegistre("col_tutelats_fi"));
+		
 		//obrim el excell
 		Workbook workbook = Workbook.getWorkbook(new File(path));
 		//obtenim la primera pagina
 		Sheet sheet = workbook.getSheet(0);
-		//AlumnesTutors 7-8
-		//Professor 9
-		//Tutelats 11-26
 		//recorre files
 		for(int i = 0; i<sheet.getRows();i++){
 			//recorre columnes
-			for(int j = 7;j<=26;j++){
+			for(int j = col_tutor1;j<=col_tutelats_fi;j++){
 				String nif = sheet.getCell(i, j).getContents();
-				if(j<9){
+				if(j<col_prof){
 					AlumneTutor tut = bll.buscarAlumneTutor(nif);
 					mapa.sumarAssistencia(tut);
 				}else{
-					if(j==9){
+					if(j==col_prof){
 						Professor prof = bll.buscarProfessor(nif);
 						mapa.sumarAssistencia(prof);
 					}else{
-						//no ens interesa la columna, no hi ha professor 2 
-						if(j>=11){
+						//no ens interesa la columna 10, no hi ha professor 2 
+						if(j>=col_tutelats_ini){
 							Tutelat tute = bll.buscarTutelat(nif);
 							mapa.sumarAssistencia(tute);
 						}
